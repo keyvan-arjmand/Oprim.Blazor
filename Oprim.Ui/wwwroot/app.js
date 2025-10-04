f91596062275c1f0ba151546928568ae2bc88373
k1 <keyvan.arjmnd@gmail.com>
2025-10-04 17:49:43 +0330

    kkkk

--- Oprim.Ui/wwwroot/app.js
+++ Oprim.Ui/wwwroot/app.js
@@ -1,11 +1,3 @@
 window.loadingDashboard = () => {
-    function showLoader() {
-        $("#global-loader").fadeIn("slow");
-    }
 
-    function hideLoader() {
-        $("#global-loader").fadeOut("slow");
-    }
-
-    hideLoader();
 };
